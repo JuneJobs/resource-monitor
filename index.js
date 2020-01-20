9ffda171eb67849d26a19351834c600964673bbf
junejobs <j.jobs1028@gmail.com>
2020-01-20 22:48:21 +0900

    Add Server resource monitor function

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-'use strict';;
+'use strict';
+let os = require('os');
+
+
 let bit_to_megabyte = (bit) => {
     let megabyte = (bit / 8388608).toFixed(2);
     return megabyte;
 }
 
-let monitor = () => {
-    let measured_resources = process.memoryUsage();
-    let rss = bit_to_megabyte(measured_resources.rss),
-        heapTotal = bit_to_megabyte(measured_resources.heapTotal), 
-        heapUsed = bit_to_megabyte(measured_resources.heapUsed),
-        result = {};
 
-    result = JSON.stringify({
+
+let monitor = () => {
+    let measured_process_resource = process.memoryUsage();
+    let rss = bit_to_megabyte(measured_process_resource.rss),
+        heapTotal = bit_to_megabyte(measured_process_resource.heapTotal), 
+        heapUsed = bit_to_megabyte(measured_process_resource.heapUsed),
+        process_resource_result = {},
+        system_resource_result = {};
+
+    process_resource_result = JSON.stringify({
         rss: rss,
         heapTotal: heapTotal,
         heapUsed: heapUsed
     });
 
-    console.log(`프로세스 자원 사용량 확인: \n${result}`);
+    system_resource_result = JSON.stringify({
+        cpu: os.cpus(),
+        total_memory: os.totalmem(),
+        free_memory: os.freemem()
+    });
+    
+
+    console.log(`프로세스 자원 사용량: \n${process_resource_result}`);
+    console.log(`서버 자원 사용량: \n ${system_resource_result}`);
 }
 
 setInterval(monitor, 1000);
