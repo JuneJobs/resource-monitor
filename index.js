bb90ff37b301b9500ea4baab0ee466db3c0c1949
junejobs <j.jobs1028@gmail.com>
2020-01-21 18:12:32 +0900

    Update server resource monitoring code. module os -> node_os_utils. BETTER!

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 'use strict';
-let os = require('os');
-
+let osu = require('node-os-utils'),
+    cpu = osu.cpu,
+    mem = osu.mem;
 
 let bit_to_megabyte = (bit) => {
     let megabyte = (bit / 8388608).toFixed(2);
     return megabyte;
 }
 
+let get_server_resource = async () => {
+    let cpu_usage = await cpu.usage(),
+        mem_usage = await mem.info();
+        return {
+            cpu_usage: cpu_usage, 
+            mem_usage: mem_usage
+        };
+}
 
 
-let monitor = () => {
+let monitor = async () => {
     let measured_process_resource = process.memoryUsage();
     let rss = bit_to_megabyte(measured_process_resource.rss),
         heapTotal = bit_to_megabyte(measured_process_resource.heapTotal), 
@@ -23,13 +32,8 @@ let monitor = () => {
         heapUsed: heapUsed
     });
 
-    system_resource_result = JSON.stringify({
-        cpu: os.cpus(),
-        total_memory: os.totalmem(),
-        free_memory: os.freemem()
-    });
-    
-
+    system_resource_result = await JSON.stringify(get_server_resource);
+    system_resource_result;
     console.log(`프로세스 자원 사용량: \n${process_resource_result}`);
     console.log(`서버 자원 사용량: \n ${system_resource_result}`);
 }
