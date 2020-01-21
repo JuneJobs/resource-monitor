c759086bfc28875ed12cb34669b5ddf826bfa5f9
junejobs <j.jobs1028@gmail.com>
2020-01-22 02:18:53 +0900

    Update Monitoring resource about server, process

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ let bit_to_megabyte = (bit) => {
     return megabyte;
 }
 
+let get_process_resource = () => {
+    let measured_process_resource = process.memoryUsage(),
+        rss = bit_to_megabyte(measured_process_resource.rss),
+        heapTotal = bit_to_megabyte(measured_process_resource.heapTotal), 
+        heapUsed = bit_to_megabyte(measured_process_resource.heapUsed);
+    return {
+        rss: rss,
+        heapTotal: heapTotal,
+        heapUsed: heapUsed
+    };
+        
+};
+
 let get_server_resource = async () => {
     let cpu_usage = await cpu.usage(),
         mem_usage = await mem.info();
@@ -15,27 +28,18 @@ let get_server_resource = async () => {
             cpu_usage: cpu_usage, 
             mem_usage: mem_usage
         };
-}
-
+};
 
 let monitor = async () => {
-    let measured_process_resource = process.memoryUsage();
-    let rss = bit_to_megabyte(measured_process_resource.rss),
-        heapTotal = bit_to_megabyte(measured_process_resource.heapTotal), 
-        heapUsed = bit_to_megabyte(measured_process_resource.heapUsed),
-        process_resource_result = {},
+    let process_resource_result = {},
         system_resource_result = {};
 
-    process_resource_result = JSON.stringify({
-        rss: rss,
-        heapTotal: heapTotal,
-        heapUsed: heapUsed
-    });
+    process_resource_result = JSON.stringify(get_process_resource());
 
-    system_resource_result = await JSON.stringify(get_server_resource);
-    system_resource_result;
-    console.log(`프로세스 자원 사용량: \n${process_resource_result}`);
-    console.log(`서버 자원 사용량: \n ${system_resource_result}`);
+    system_resource_result =  await get_server_resource();
+    system_resource_result = JSON.stringify(system_resource_result);
+    console.log(`process resource usage: \n${process_resource_result}`);
+    console.log(`pserver resource usage: \n ${system_resource_result}`);
 }
 
 setInterval(monitor, 1000);
